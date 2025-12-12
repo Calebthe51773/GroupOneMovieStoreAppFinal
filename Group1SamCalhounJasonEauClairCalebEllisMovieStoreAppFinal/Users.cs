5a6d039eff768c67b1696e11425951709b18ab70
soupe <soupe@DESKTOP-OSLIJU5>
2025-12-11 21:49:15 -0600

    Add project files.

---
+++ Group1SamCalhounJasonEauClairCalebEllisMovieStoreAppFinal/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupOneMovieStoreAppFinal
+{
+    public class Users
+    {
+        public Users(){}
+        public Users(string firstName, string lastName, string contactInfo) => (this.FirstName, this.LastName, this.ContactInfo) = (firstName, lastName, contactInfo);
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string ContactInfo { get; set; } = "";
+        public string GetUserInfo() => $"{FirstName} {LastName}, {ContactInfo}";
+    }
+}
